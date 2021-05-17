9ad339d3d282218cbe1cab8f880ae34b08a18138
nishant-kumar1 <joshmacgrathsit@gmail.com>
2021-05-17 17:39:06 +0530

    new change

--- mymavenproject/src/main/java/com/data/App.java
+++ mymavenproject/src/main/java/com/data/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello welcome");
+		System.out.println("Hello welcome again");
 	}
 
 }
